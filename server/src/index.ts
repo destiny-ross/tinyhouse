939773c7e03ece322d817a803f80b11669cf649f
Destiny Ross <destinylaross@gmail.com>
2021-09-29 10:33:48 -0400

    converted Express REST server to simple GraphQL server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,29 +1,20 @@
 import express from "express";
-import {listings} from './listings';
-
-const app = express();
-
+import {ApolloServer} from 'apollo-server-express';
+import { typeDefs, resolvers } from './graphql/';
 
 const port: number = 9000;
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+const startExpressApolloServer = async () => {
+  const app = express();
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+  server.applyMiddleware({app, path: '/api'})
+  
+  
+  app.listen(port, () => {
+    console.log(`[app]: http://localhost:${port}`);
+  });
+  
+}
 
-app.get('/listings', (_req, res) => {
-  return res.send(listings)
-})
-
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++){
-    if (listings[i].id === id){
-      return res.send(listings.splice(i, 1));
-    }
-  }
-  return res.send("failed to delete listing");
-});
-
-app.listen(port);
-
-console.log(`[app]: http://localhost:${port}`);
+startExpressApolloServer();
