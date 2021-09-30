722ccb1df98004b9e3dfd1a744cfa05092e389c4
Destiny Ross <destinylaross@gmail.com>
2021-09-30 17:06:20 -0400

    connected to MongoDB Atlas database, added dotenv package for configuring environment variables and personal data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,23 @@
+require('dotenv').config();
+
 import express from "express";
 import {ApolloServer} from 'apollo-server-express';
 import { typeDefs, resolvers } from './graphql/';
-
-const port: number = 9000;
+import { connectDatabase } from "./database";
 
 const startExpressApolloServer = async () => {
+  const db = await connectDatabase();
   const app = express();
   const server = new ApolloServer({ typeDefs, resolvers });
   await server.start();
   server.applyMiddleware({app, path: '/api'})
   
   
-  app.listen(port, () => {
-    console.log(`[app]: http://localhost:${port}`);
+  app.listen(process.env.PORT, () => {
+    console.log(`[app]: http://localhost:${process.env.PORT}`);
   });
-  
+  const listings = await db.listings.find({}).toArray();
+  console.log(listings);
 }
 
 startExpressApolloServer();
