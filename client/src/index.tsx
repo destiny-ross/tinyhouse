7d0d929660a5afee593ff3dcd350e622dc83c4ad
Destiny Ross <destinylaross@gmail.com>
2021-10-24 23:07:56 -0400

    set up react routing, creating simple components for each anticipated route that displays text. master css file.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Listings } from './sections';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home, Host, Listing, Listings, NotFound, User } from './sections';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import './styles/index.css';
 
 const client = new ApolloClient({ uri: '/api', cache: new InMemoryCache() });
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route exact path="/user/:id" component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <React.StrictMode>
-      <Listings title="Tinyhouse Listings" />
-    </React.StrictMode>
+    <App />
   </ApolloProvider>,
   document.getElementById('root')
 );
