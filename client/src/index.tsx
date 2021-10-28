3454948adf3172a4c18b8969dc64c14b99658aca
Destiny Ross <destinylaross@gmail.com>
2021-10-28 01:35:19 -0400

    created server/src/lib/utils folder for shared code and set up client to send a X-CSRF token to server on each request for consumption by authorize utility

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Affix, Layout, Spin } from 'antd';
 import {
   AppHeader,
   Home,
@@ -11,12 +12,38 @@ import {
   NotFound,
   User,
 } from './sections';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import { Affix, Layout } from 'antd';
+import { AppHeaderSkeleton, ErrorBanner } from './lib/components';
+import { LOG_IN } from './lib/graphql/mutations';
+import {
+  LogIn as LogInData,
+  LogInVariables,
+} from './lib/graphql/mutations/LogIn/__generated__/LogIn';
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  useMutation,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Viewer } from './lib/types';
 import './styles/index.css';
 
-const client = new ApolloClient({ uri: '/api', cache: new InMemoryCache() });
+const httpLink = new HttpLink({ uri: '/api' });
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      'X-CSRF-TOKEN': sessionStorage.getItem('token') || '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+});
 
 const initialViewer: Viewer = {
   id: null,
@@ -28,6 +55,36 @@ const initialViewer: Viewer = {
 
 const App = () => {
   const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  const [logIn, { error }] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+    onCompleted: (data) => {
+      if (data && data.logIn) {
+        setViewer(data.logIn);
+
+        if (data.logIn.token) {
+          sessionStorage.setItem('token', data.logIn.token);
+        } else {
+          sessionStorage.removeItem('token');
+        }
+      }
+    },
+  });
+  const logInRef = useRef(logIn);
+
+  useEffect(() => {
+    logInRef.current();
+  }, []);
+
+  if (!viewer.didRequest && !error) {
+    <Layout className="app-skeleton">
+      <AppHeaderSkeleton />
+      <div className="app-skeleton__spin-section">
+        <Spin size="large" tip="Launching Tinyhouse" />
+      </div>
+    </Layout>;
+  }
+  const logInErrorBannerElement = error ? (
+    <ErrorBanner description="We weren't able to verify if you were logged in. Please try again later." />
+  ) : null;
 
   return (
     <Router>
