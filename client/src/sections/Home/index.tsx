6ba4f822490039cf17934464dd938c851cb66c69
Destiny Ross <destinylaross@gmail.com>
2021-11-03 00:53:39 -0400

    completed home page ui by adding premium listing section

--- client/src/sections/Home/index.tsx
+++ client/src/sections/Home/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
 import { Col, Row, Layout, Typography } from 'antd';
+import { LISTINGS } from '../../lib/graphql/queries';
+import {
+  Listings as ListingsData,
+  ListingsVariables,
+} from '../../lib/graphql/queries/Listings/__generated__/Listings';
+import { ListingsFilter } from '../../lib/graphql/globalTypes';
 import { displayErrorMessage } from '../../lib/utils';
-import { HomeHero } from './components';
+import { HomeHero, HomeListings, HomeListingsSkeleton } from './components';
 
 import mapBackground from './assets/map-background.jpg';
 import sanFransiscoImage from './assets/san-fransisco.jpg';
@@ -11,22 +18,55 @@ import cancunImage from './assets/cancun.jpg';
 const { Content } = Layout;
 const { Paragraph, Title } = Typography;
 
+const PAGE_LIMIT = 4;
+const PAGE_NUMBER = 1;
+
 export const Home = ({ history }: RouteComponentProps) => {
+  const { loading, data } = useQuery<ListingsData, ListingsVariables>(
+    LISTINGS,
+    {
+      variables: {
+        filter: ListingsFilter.PRICE_HIGH_TO_LOW,
+        limit: PAGE_LIMIT,
+        page: PAGE_NUMBER,
+      },
+    }
+  );
+
   const onSearch = (value: string) => {
     const trimmedValue = value.trim();
 
     if (trimmedValue) {
       history.push(`/listings/${trimmedValue}`);
     } else {
-      displayErrorMessage('Please enter a valid search.');
+      displayErrorMessage('Please enter a valid search!');
     }
   };
+
+  const renderListingsSection = () => {
+    if (loading) {
+      return <HomeListingsSkeleton />;
+    }
+
+    if (data) {
+      return (
+        <HomeListings
+          title="Premium Listings"
+          listings={data.listings.result}
+        />
+      );
+    }
+
+    return null;
+  };
+
   return (
     <Content
       className="home"
       style={{ backgroundImage: `url(${mapBackground})` }}
     >
       <HomeHero onSearch={onSearch} />
+
       <div className="home__cta-section">
         <Title level={2} className="home__cta-section-title">
           Your guide for all things rental
@@ -42,6 +82,8 @@ export const Home = ({ history }: RouteComponentProps) => {
           Popular listings in the United States
         </Link>
       </div>
+
+      {renderListingsSection()}
 
       <div className="home__listings">
         <Title level={4} className="home__listings-title">
