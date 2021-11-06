b60f3f1e0155a689285c8aaa38e61caa90b690b7
Destiny Ross <destinylaross@gmail.com>
2021-11-06 09:23:00 -0500

    cloudinary image upload for improved performance

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -1,7 +1,7 @@
 import { IResolvers } from '@graphql-tools/utils';
 import { Request } from 'express';
 import { ObjectId } from 'mongodb';
-import { Google } from '../../../lib/api';
+import { Google, Cloudinary } from '../../../lib/api';
 import { Database, Listing, ListingType, User } from '../../../lib/types';
 import { authorize } from '../../../lib/utils';
 import {
@@ -128,9 +128,12 @@ export const listingResolvers: IResolvers = {
         throw new Error('invalid address input');
       }
 
+      const imageUrl = await Cloudinary.upload(input.image);
+
       const insertResult = await db.listings.insertOne({
         _id: new ObjectId(),
         ...input,
+        image: imageUrl,
         bookings: [],
         bookingsIndex: {},
         country,
